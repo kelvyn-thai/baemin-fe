c6a1bfbacd56d60f552fdfbd36be46265b6f4e3e
Jayce <thainguyenhoangphatit@gmail.com>
2022-10-06 13:37:24 +0700

    Test deploy gh-pages from master

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -5,7 +5,6 @@ const HomePage: React.FC = () => {
   const [state, setState] = React.useState();
   return (
     <div>
-      This is home page
       <div>
         <img src={`${ENV.ASSETS_PATH}/images/banner.png`} alt="banner" />
       </div>
