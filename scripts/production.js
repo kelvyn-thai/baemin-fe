3f3f02a1718ad924a9800eeff8147298481e1bea
Jayce <thainguyenhoangphatit@gmail.com>
2022-10-20 14:17:32 +0700

    Remove unusded css

--- scripts/production.js
+++ scripts/production.js
@@ -1,6 +1,18 @@
+const path = require("path");
+const glob = require("glob");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const PurgeCSSPlugin = require("purgecss-webpack-plugin").PurgeCSSPlugin;
+
+const PATHS = {
+  src: path.join(__dirname, "../src"),
+};
 
 module.exports = function override(config, env) {
+  config.plugins.push(
+    new PurgeCSSPlugin({
+      paths: glob.sync(`${PATHS.src}/**/*`, { nodir: true }),
+    })
+  );
   config.optimization.minimizer.push(
     new UglifyJsPlugin({
       uglifyOptions: {
