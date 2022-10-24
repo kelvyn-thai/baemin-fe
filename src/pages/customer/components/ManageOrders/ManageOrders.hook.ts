b967aa43ad0a1bbafef93ecebc044d3c6f6607e2
Jayce <thainguyenhoangphatit@gmail.com>
2022-10-24 09:39:41 +0700

    Update socket io

--- src/pages/customer/components/ManageOrders/ManageOrders.hook.ts
+++ src/pages/customer/components/ManageOrders/ManageOrders.hook.ts
@@ -1,5 +1,14 @@
 import { useQuery, UseQueryResult } from "@tanstack/react-query";
-import { getAllOrders, OrderType } from "pages/order/components/Order";
+import { useAuthenStore } from "pages/authen";
+import {
+  getAllOrders,
+  OrderType,
+  useMutationUpdateOrder,
+} from "pages/order/components/Order";
+import React from "react";
+import socket from "socket-io";
+import { delay } from "utils/delay";
+import { randomInRange } from "utils/random";
 
 export const useQueryOrderHistory: () => UseQueryResult<
   OrderType[],
@@ -10,4 +19,42 @@ export const useQueryOrderHistory: () => UseQueryResult<
     queryFn: () => getAllOrders(),
   });
   return query;
+};
+
+export const useSocketMerchantUpdateOrderStatus = () => {
+  const { userInfo } = useAuthenStore();
+  const { id: userId } = userInfo;
+  const mutationUpdateOrder = useMutationUpdateOrder();
+  React.useEffect(() => {
+    socket.on(
+      `updated-order-status-to-customer-${userId}`,
+      async (order: OrderType) => {
+        const orderPhase1 = await mutationUpdateOrder.mutateAsync(order);
+        await delay(randomInRange(1000, 5000));
+        const orderPhase2 = await mutationUpdateOrder.mutateAsync({
+          ...orderPhase1,
+          status: "DriverAssigned",
+          updatedTime: new Date(new Date().toUTCString()).toString(),
+        });
+        socket.emit("assign-order-to-driver", orderPhase2);
+      }
+    );
+  }, []);
+};
+
+export const useSocketDriverUpdateOrderStatus = () => {
+  const { userInfo } = useAuthenStore();
+  const { id: userId } = userInfo;
+  const mutationUpdateOrder = useMutationUpdateOrder();
+  React.useEffect(() => {
+    socket.on(`driver-delivering-order-${userId}`, async (order: OrderType) => {
+      await mutationUpdateOrder.mutateAsync(order);
+      await delay(randomInRange(1000, 5000));
+      await mutationUpdateOrder.mutateAsync({
+        ...order,
+        status: "Done",
+        updatedTime: new Date(new Date().toUTCString()).toString(),
+      });
+    });
+  }, []);
 };
--- src/pages/home/HomePage.Customer.tsx
+++ src/pages/home/HomePage.Customer.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import CategoryList from "pages/category/components/CategoryList";
+import {
+  useSocketDriverUpdateOrderStatus,
+  useSocketMerchantUpdateOrderStatus,
+} from "pages/customer/components/ManageOrders/ManageOrders.hook";
 
 import(/* webpackPrefetch: true */ "pages/category/components/CategoryList");
 
-const HomePageCustomer: React.FC = () => (
-  <div className="home-page">
-    <CategoryList />
-  </div>
-);
+const HomePageCustomer: React.FC = () => {
+  useSocketMerchantUpdateOrderStatus();
+  useSocketDriverUpdateOrderStatus();
+  return (
+    <div className="home-page">
+      <CategoryList />
+    </div>
+  );
+};
 
 export default React.memo(HomePageCustomer);
