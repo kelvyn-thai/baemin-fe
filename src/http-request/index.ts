5b09981c6a1ee8d1dd9731079d4a633b4ab52bf7
Jayce <thainguyenhoangphatit@gmail.com>
2022-10-26 11:20:38 +0700

    HTTP request handle authorize

--- src/http-request/index.ts
+++ src/http-request/index.ts
@@ -1,3 +1,25 @@
+import { delay } from "utils/delay";
+
+const defaultRequestInit: RequestInit = {
+  cache: "no-cache",
+  mode: "cors",
+  headers: {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers": "*",
+  },
+  credentials: "same-origin", // include, *same-origin, omit
+  redirect: "follow", // manual, *follow, error
+  referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+};
+
+const handleHTTPRequestReject = (reason: any) => {
+  throw new Error(reason);
+};
+
+const handleHTTPRequestResolve = (value: Response) => value.json();
+
 class HTTP {
   baseURL: string;
 
@@ -5,91 +27,63 @@ class HTTP {
 
   requestInit: RequestInit;
 
+  accessToken: string;
+
   constructor({ baseURL }: { baseURL: string }) {
     this.baseURL = baseURL;
     this.controller = new AbortController();
-    this.requestInit = {
-      cache: "no-cache",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "*",
-        "Access-Control-Allow-Headers": "*",
-      },
-      credentials: "same-origin", // include, *same-origin, omit
-      redirect: "follow", // manual, *follow, error
-      referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+    this.accessToken = "";
+    this.requestInit = defaultRequestInit;
+  }
+
+  async handleUnAuthorize() {
+    await delay(1000);
+    this.accessToken = "some_thing_magic";
+    this.requestInit.headers = {
+      ...defaultRequestInit.headers,
+      Authorization: `Bearer ${this.accessToken}`,
     };
   }
 
-  get(url: string) {
-    const { signal } = this.controller;
-    return fetch(`${this.baseURL}/${url}`, {
-      ...this.requestInit,
-      method: "GET",
-      signal,
-    })
-      .then((res) => res.json())
-      .catch((error) => {
-        throw new Error(error);
+  async request(params: { url: string; body?: any; method: string }) {
+    try {
+      const { url, body, method } = params;
+      const { signal } = this.controller;
+      // handle response
+      const response = await fetch(`${this.baseURL}/${url}`, {
+        ...this.requestInit,
+        method,
+        signal,
+        body,
       });
+      if (response.status === 401) {
+        await this.handleUnAuthorize(); // handle authorize
+        await this.request(params);
+      }
+      return handleHTTPRequestResolve(response);
+    } catch (error) {
+      return handleHTTPRequestReject(error);
+    }
+  }
+
+  get(url: string) {
+    return this.request({ url, method: "GET" });
   }
 
   post(url: string, data?: any) {
-    const { signal } = this.controller;
-    return fetch(`${this.baseURL}/${url}`, {
-      ...this.requestInit,
-      method: "POST", // *GET, POST, PUT, DELETE, etc.
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-      signal,
-    })
-      .then((res) => res.json())
-      .catch((error) => {
-        throw new Error(error);
-      });
+    return this.request({ url, body: data, method: "POST" });
   }
 
   put(url: string, data?: any) {
-    const { signal } = this.controller;
-    return fetch(`${this.baseURL}/${url}`, {
-      ...this.requestInit,
-      method: "PUT", // *GET, POST, PUT, DELETE, etc.
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-      signal,
-    })
-      .then((res) => res.json())
-      .catch((error) => {
-        throw new Error(error);
-      });
+    return this.request({ url, body: data, method: "PUT" });
   }
 
   patch(url: string, data?: any) {
-    const { signal } = this.controller;
-    return fetch(`${this.baseURL}/${url}`, {
-      ...this.requestInit,
-      method: "PATCH", // *GET, POST, PUT, DELETE, etc.
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-      signal,
-    })
-      .then((res) => res.json())
-      .catch((error) => {
-        throw new Error(error);
-      });
+    return this.request({ url, body: data, method: "PATCH" });
   }
 
   delete(url: string, data?: any) {
-    const { signal } = this.controller;
-    return fetch(`${this.baseURL}/${url}`, {
-      ...this.requestInit,
-      method: "DELETE", // *GET, POST, PUT, DELETE, etc.
-      body: JSON.stringify(data), // body data type must match "Content-Type" header
-      signal,
-    })
-      .then((res) => res.json())
-      .catch((error) => {
-        throw new Error(error);
-      });
+    return this.request({ url, body: data, method: "DELETE" });
   }
 
   abort() {
